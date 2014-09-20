535e5c5b4288970be08fa43ab572e740b7122b6b
Igal Masliansky <igor.masliansky@gmail.com>
2014-09-21 00:39:49 +0300

    twangular

--- twangular/app/scripts/controller/mainCtrl.js
+++ twangular/app/scripts/controller/mainCtrl.js
@@ -6,6 +6,35 @@
         function ($scope,dataSourceService) {
             $scope.d = {};
 
+            var oauth = new OAuth({
+                consumer: {
+                    public: 'xvz1evFS4wEEPTGEFPHBog',
+                    secret: 'kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw'
+                },
+                signature_method: 'HMAC-SHA1'
+            });
+
+            var request_data = {
+                url: 'https://api.twitter.com/oauth/request_token',
+                method: 'POST',
+                data: {
+                    status: 'Hello Ladies + Gentlemen, a signed OAuth request!'
+                }
+            };
+
+            var token = {
+                public: '370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb',
+                secret: 'LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE'
+            };
+
+            $.ajax({
+                url: request_data.url,
+                type: request_data.method,
+                data: oauth.authorize(request_data, token)
+            }).done(function(data) {
+                //process your data here
+            });
+
             $scope.d.twitterLogIn = function(){
                 dataSourceService.logInWithTwitter();
             };
--- twangular/app/scripts/service/dataSourceService.js
+++ twangular/app/scripts/service/dataSourceService.js
@@ -7,30 +7,34 @@
             var self = this;
 
             self.logInWithTwitter = function(){
-                var oauthReqTokenUrl = 'http://api.twitter.com/oauth/request_token';
+                var oauthReqTokenUrl = 'https://api.twitter.com/oauth/request_token';
                 var headers = {
-                    Authorization:{
-                        'OAuth oauth_callback':"http%3A%2F%2Flocalhost%2Fsign-in-with-twitter%2F",
-                        oauth_consumer_key:"cChZNFj6T5R0TigYB9yd1w",
-                        oauth_nonce:"ea9ec8429b68d6b77cd5600adbbb0456",
-                        oauth_signature:"F1Li3tvehgcraF8DMJ7OyxO4w9Y%3D",
-                        oauth_signature_method:"HMAC-SHA1",
-                        oauth_timestamp:"1318467427",
-                        oauth_version:"1.0"
-                    }
-                };
+                    Authorization: 'OAuth ' +
+                        'oauth_nonce="' + (new Date()).getTime() + '", ' +
+                        'oauth_callback="http%3A%2F%2Fmyapp.com%3A3005%2Ftwitter%2Fprocess_callback", ' +
+                        'oauth_signature_method="HMAC-SHA1", ' +
+                        'oauth_timestamp="' + (new Date()).getTime() + '", ' +
+                        'oauth_consumer_key="jdoHhwX1IjmMvZanpSU5TxSyK", ' +
+                        'oauth_signature="Pc%2BMLdv028fxCErFyi8KXFM%2BddU%3D", ' +
+                        'oauth_version="1.0"'
+                    };
+/*
+                var data = {
+                    oauth_callback: "//http%3A%2F%2Figormas.github.io%2Ftwangular%2Fapp%2Findex.html",
+                    oauth_consumer_key:"L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg",
+                    oauth_nonce:"ea9ec8429b68d6b77cd5600adbbb0456",
+                    oauth_signature:"F1Li3tvehgcraF8DMJ7OyxO4w9Y%3D",
+                    oauth_signature_method:"HMAC-SHA1",
+                    oauth_timestamp:"1318467427",
+                    oauth_version:"1.0"
+                };*/
 
+                var prom = $http({
+                    url:oauthReqTokenUrl,
+                    method: 'POST',
+                    headers: headers
 
-/*                var prom = $http({
-                    url: oauthReqTokenUrl,
-                    method:"POST",
-                    headers: {
-                        'Authorization': 'Basic dGVzdDp0ZXN0',
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-                });*/
-                /*.post(oauthReqTokenUrl,undefined,{headers: headers});*/
-                var prom = $http.post(oauthReqTokenUrl,undefined,{headers: headers});
+                });
                 prom.then(function(res){
 
                 });
