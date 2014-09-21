c9e4207bf6d787458f43ebaac64db2ceb3ef9523
Igal Masliansky <igor.masliansky@gmail.com>
2014-09-21 23:53:20 +0300

    logged user page was added

--- twangular/app/scripts/service/dataSourceService.js
+++ twangular/app/scripts/service/dataSourceService.js
@@ -7,17 +7,22 @@
             var self = this;
 
             self.logInWithTwitter = function(){
-                var oauthReqTokenUrl = 'https://api.twitter.com/oauth/request_token';
+                var url = 'https://api.twitter.com/oauth/request_token';
                 var currTimeInSeconds = (new Date()).getTime();
+                var httpMethod = 'POST';
+                var consumerSecret = 'L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg';
                 var parameters = {
-                    oauth_nonce: currTimeInSeconds,
-                    oauth_callback: "http%3A%2F%2Figormas.github.io%2Ftwangular%2Fapp%2Findex.html",
+                    oauth_callback: "http%3A%2F%2Flocalhost%2Fsign-in-with-twitter%2F",
+                    oauth_consumer_key: "cChZNFj6T5R0TigYB9yd1w",
+                    oauth_nonce: 'ea9ec8429b68d6b77cd5600adbbb0456',
                     oauth_signature_method: "HMAC-SHA1",
-                    oauth_timestamp: currTimeInSeconds ,
-                    oauth_consumer_key: "jdoHhwX1IjmMvZanpSU5TxSyK",
+                    oauth_timestamp: '1318467427',
                     oauth_version: "1.0"
                 };
-                var ncodedSignature = oauthSignature.generate('POST', oauthReqTokenUrl, parameters);
+                var encodedSignature = oauthSignature.generate(httpMethod, url, parameters,consumerSecret);
+                console.log(encodedSignature);
+                var signature = oauthSignature.generate(httpMethod, url, parameters,consumerSecret);
+                console.log(signature);
 
                 /*var headers = {
                     Authorization: 'OAuth ' +
